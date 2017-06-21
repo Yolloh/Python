ab9d8c6c68ac96a2584314cd986a957e7c41c508
Pauline Karlsson <parro_88@hotmail.com>
2017-06-21 18:35:49 +0200

    Add files via upload
    added a file where I used the Pyrhon turtle module

---
+++ turtle().py
@@ -0,0 +1,51 @@
+import turtle
+
+def triangles():
+    window = turtle.Screen()
+    bo = turtle.Turtle()
+
+    bo.penup()
+    bo.setposition(0, 50)
+    bo.pendown()
+    for i in range(0, 3):
+        bo.right(120)
+        bo.forward(100)
+
+    bo.penup()
+    bo.setposition(0, -125)
+    bo.pendown()
+    for i in range(0, 3):
+        bo.left(120)
+        bo.forward(100)
+    window.exitonclick()
+triangles()
+
+
+
+def square(x):                              # This functions draws a square
+    for i in range(1,5):
+        x.forward(100)
+        x.right(90)
+
+
+def circle():
+    window = turtle.Screen()
+    window.bgcolor("lightblue")
+
+    emma = turtle.Turtle()
+    emma.shape("turtle")
+    emma.color("white")
+
+    for i in range(1,6):                # This loop repeats the function and tilts the angle for every time
+        square(emma)
+        emma.right(10)
+    window.exitonclick()
+circle()
+
+
+
+
+
+
+
+
